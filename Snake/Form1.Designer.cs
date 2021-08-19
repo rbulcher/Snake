f666c9545a1e825e3747a9c3b443adfe17fb2904
rbulcher <89021504+rbulcher2@users.noreply.github.com>
2021-08-19 15:56:11 -0400

    Add project files.

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace sanek
+{
+    partial class Snake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.snakeHead = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.snakeHead)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // snakeHead
+            // 
+            this.snakeHead.BackColor = System.Drawing.Color.Transparent;
+            this.snakeHead.BackgroundImage = global::sanek.Properties.Resources.beeHead;
+            this.snakeHead.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.snakeHead.Location = new System.Drawing.Point(350, 200);
+            this.snakeHead.Name = "snakeHead";
+            this.snakeHead.Size = new System.Drawing.Size(40, 40);
+            this.snakeHead.TabIndex = 2;
+            this.snakeHead.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::sanek.Properties.Resources.button_stop;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(329, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::sanek.Properties.Resources.button_start;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(329, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Snake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.snakeHead);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.Name = "Snake";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Snake_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.snakeHead)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox snakeHead;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using sanek.Properties;
+
+namespace sanek
+{
+    public partial class Snake : Form
+    {
+        private const int SnakeSpeed = 40;
+        private Point _direction;
+        private bool _fruitInGame;
+        private bool _inGame;
+        private readonly ArrayList _snakeBody = new ArrayList();
+        private PictureBox _fruit;
+        private bool _colorYellow;
+
+        public Snake()
+        {
+            InitializeComponent();
+            button2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _inGame = true;
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            _colorYellow = false;
+            button1.Visible = false;
+            button2.Visible = true;
+            _direction = new Point(SnakeSpeed, 0);
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (_inGame)
+            {
+                MoveSnake();
+                if(!_fruitInGame)
+                    AddFruit();
+            }
+        }
+
+        private Image randomFruit()
+        {
+            var r = new Random();
+            var select = r.Next(3);
+            Image randomFruit;
+
+            switch (select)
+            {
+                case 0:
+                    randomFruit = Resources.banana;
+                    break;
+                case 1:
+                    randomFruit = Resources.strawb;
+                    break;
+                case 2:
+                    randomFruit = Resources.waterMelon;
+                    break;
+                default:
+                    randomFruit = Resources.banana;
+                    break;
+            }
+            return randomFruit;
+        }
+
+        private void AddFruit()
+        {
+                _fruitInGame = true;
+                var r = new Random();
+                var x = r.Next(Width - 80);
+                var y = r.Next(Height - 80);
+                _fruit = new PictureBox
+                {
+                    Location = new Point(x, y),
+                    BackgroundImage = randomFruit(),
+                    BackgroundImageLayout = BackgroundImageLayout = ImageLayout.Stretch,
+                    Size = new Size(40, 40),
+                    Text = @"fruit"
+                };
+                Controls.Add(_fruit);
+        }
+
+        private void MoveSnake()
+        {
+            var lastPos = snakeHead.Location;
+
+            snakeHead.Location = new Point(snakeHead.Location.X + _direction.X, snakeHead.Location.Y + _direction.Y);
+            CheckCollision();
+            foreach (PictureBox bodyPiece in _snakeBody)
+            {
+                var curPos = bodyPiece.Location;
+                bodyPiece.Location = lastPos;
+                lastPos = curPos;
+            }
+        }
+
+        private void CheckCollision()
+        {
+            CollisionFruit();
+            if (_snakeBody.Cast<PictureBox>().Any(body => snakeHead.Bounds.IntersectsWith(body.Bounds)) || (snakeHead.Location.X + 40 >= Width || snakeHead.Location.X <= 0 || snakeHead.Location.Y + 40 >= Height ||
+                snakeHead.Location.Y <= 0))
+            {
+                _inGame = false;
+                timer1.Stop();
+                MessageBox.Show(@"You died.");
+                Reset();
+                
+            }
+        }
+
+        private void CollisionFruit()
+        {
+            if (_fruitInGame)
+            {
+                if (snakeHead.Bounds.IntersectsWith(_fruit.Bounds))
+                {
+                    _snakeBody.Add(AddBody());
+                    this.Controls.Remove(_fruit);
+                    
+                    _fruitInGame = false;
+                }
+            }
+        }
+
+        private PictureBox AddBody()
+        {
+            _colorYellow = !_colorYellow;
+            PictureBox body;
+            if (_colorYellow)
+            {
+                body = new PictureBox { BackgroundImage = Resources.blackCircle, Text = @"body", Size = new Size(40, 40), BackgroundImageLayout = ImageLayout.Stretch, BackColor = Color.Transparent };
+                this.Controls.Add(body);
+
+            }
+            else
+            {
+                 body = new PictureBox { BackgroundImage = Resources.yellowCircle, Text = @"body", Size = new Size(40, 40), BackgroundImageLayout = ImageLayout.Stretch, BackColor = Color.Transparent };
+                this.Controls.Add(body);
+            }
+           
+            return body;
+        }
+
+        private void Snake_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (_inGame)
+                switch (e.KeyCode)
+                {
+                    case Keys.W:
+                        _direction = new Point(0, -SnakeSpeed);
+                        break;
+                    case Keys.A:
+                        _direction = new Point(-SnakeSpeed, 0);
+                        break;
+                    case Keys.S:
+                        _direction = new Point(0, SnakeSpeed);
+                        break;
+                    case Keys.D:
+                        _direction = new Point(SnakeSpeed, 0);
+                        break;
+                }
+        }
+
+        private void Reset()
+        {
+            timer1.Stop();
+            foreach (PictureBox bodyPiece in _snakeBody)
+            {
+                this.Controls.Remove(bodyPiece);
+                bodyPiece.Dispose();
+            }
+            this.Controls.Remove(_fruit);
+            snakeHead.Location = new Point(350, 200);
+            _fruitInGame = false;
+            _snakeBody.Clear();
+            _inGame = false;
+            button2.Visible = false;
+            button1.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+    }
+}
